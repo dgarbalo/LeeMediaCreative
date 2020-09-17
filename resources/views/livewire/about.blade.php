d9a67a0dcd2b54731f492c3d2cfbc9cbd9aad2c1
Devon Garbalosa <58236685+dgarbalo@users.noreply.github.com>
2020-09-16 22:25:38 -0400

    DJG working (#1)

---
+++ resources/views/livewire/about.blade.php
@@ -0,0 +1,38 @@
+<x-slot name="header">
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        {{ __('About') }}
+    </h2>
+</x-slot>
+
+<main class="lg:relative">
+    <div class="relative w-full h-64 sm:h-72 md:h-96 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 lg:h-full">
+        <img class="absolute inset-0 w-full h-full object-cover" src="/images/about_us_image.png" alt="James Lee">
+    </div>
+    <div class="mx-auto max-w-7xl w-full pt-16 pb-20 text-center lg:py-48 lg:text-left">
+        <div class="px-4 lg:w-1/2 sm:px-8 xl:pr-16">
+            <h2 class="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl lg:text-5xl xl:text-6xl">
+                Lee Media
+                <br class="xl:hidden"/>
+                <span class="text-red-700">Creative</span>
+            </h2>
+            <p class="mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">
+                Lee Media increases your business by telling your story.
+            </p>
+            <p class="mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">
+                Powerful stories compel people to take action. That action can be as simple as clicking a link or walking into a storefront, or it can be as potent as completing a sale. A good story does this by making that action a compelling reality in the mind of the viewer.
+            </p>
+            <p class="mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">
+                Quality media that is relevant to your audience and authentic to your business can accomplish powerful things. Lee Media does this through real-world business, sales and marketing experience in addition to excellent production values & disciplines.
+            </p>
+            <p class="mt-3 max-w-md mx-auto text-lg text-gray-500 sm:text-xl md:mt-5 md:max-w-3xl">
+                It’s not just about beautiful imagery (well… it is a little). It’s about imagery that has depth, meaning and real results for both you, your business, and your audience.
+            </p>
+            <br/>
+            <span class="inline-flex rounded-md shadow-sm">
+                <a href="/contact" class=" font-bold cursor-pointer inline-flex items-center px-6 py-3 border border-transparent text-base leading-6 rounded-md text-white bg-gray-600 hover:bg-gray-500 focus:outline-none focus:border-gray-700 focus:shadow-outline-gray active:bg-gray-700 transition ease-in-out duration-150">
+                    Get in touch to start telling your story.
+                </a>
+            </span>
+        </div>
+    </div>
+</main>
