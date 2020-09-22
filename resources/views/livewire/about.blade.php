53e7f1cb30932d3624079c78f7a638695acadde1
Devon Garbalosa <dgarbalo@gmail.com>
2020-09-22 08:20:52 -0400

    ADDED SRCSET FOR PICTURES

--- resources/views/livewire/about.blade.php
+++ resources/views/livewire/about.blade.php
@@ -6,7 +6,11 @@
 
 <main class="lg:relative">
     <div class="relative w-full h-64 sm:h-72 md:h-96 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 lg:h-full">
-        <img class="absolute inset-0 w-full h-full object-cover" src="/images/about_us_image.png" alt="James Lee">
+        <picture>
+            <source media="(min-width:640px)" srcset="/images/about_us_image_sm.png"/>
+            <source media="(min-width:768px)" srcset="/images/about_us_image_md.png"/>
+            <img class="absolute inset-0 w-full h-full object-cover" src="/images/about_us_image.png" alt="James Lee">
+        </picture>
     </div>
     <div class="mx-auto max-w-7xl w-full pt-16 pb-20 text-center lg:py-48 lg:text-left">
         <div class="px-4 lg:w-1/2 sm:px-8 xl:pr-16">
